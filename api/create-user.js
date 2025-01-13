e7e6ff2b8941514f604266bb42ed08de6aaf4eeb
dxkfewz <s6602041520179@email.kmutnb.ac.th>
2025-01-13 21:18:44 +0700

    New Api Update

--- api/create-user.js
+++ api/create-user.js
@@ -4,10 +4,13 @@ const API_URL = "http://192.168.1.100:8080/api/auth/local/register";
 const API_TOKEN =
   "Bearer e9cdfbe1961010d91bc79c4a52cac3a35f46791187822359ef9792a27be34bcf2aa699000b7fd9ef88036ad6d7f8c86ad0410a33807a5b968059dec5a0908803ffe86579fa1b77578608a507e508fff77ab815fa492005b584095cb992ced8dc40de4610cdd8da6c1cad548f4df85a8da2563eca3c86c67d2f8a16fceb191ac5";
 
+var name = "panuwatzx";
+var mail = "s.panuwatzx@gmail.com"
+
 const payload = {
-  username: "panuwatz",
-  email: "s.panuwatx@gmail.com",
-  password: "dekfewz121234",
+  username: name,
+  email: mail,
+  password: "dekfewz1212345",
 };
 
 axios
